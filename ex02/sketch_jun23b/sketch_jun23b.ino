5301bee401a9764d2fdf7cd7c03838e423dbfe61
duck lulu <907880522@qq.com>
2026-06-23 18:37:23 +0800

    ex02：新建目录，编写millis 1Hz闪烁代码

---
+++ ex02/sketch_jun23b/sketch_jun23b.ino
@@ -0,0 +1,19 @@
+// ex02 1Hz LED闪烁 基于millis非阻塞
+#define LED_PIN 2
+unsigned long prevTime = 0;
+const unsigned long interval = 500; // 500ms翻转一次，1Hz
+bool ledState = LOW;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  unsigned long currTime = millis();
+  // 判断是否达到间隔时间
+  if (currTime - prevTime >= interval) {
+    prevTime = currTime;
+    ledState = !ledState;
+    digitalWrite(LED_PIN, ledState);
+  }
+}
